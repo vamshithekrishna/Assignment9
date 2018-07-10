172a59aca1c4888b30c2b6f63c95553837dc2b53
vamshithekrishna <38863735+vamshithekrishna@users.noreply.github.com>
2018-07-10 19:45:53 +0530

    Update Assignment_9.1_Titanic.py

--- Assignment_9.1_Titanic.py
+++ Assignment_9.1_Titanic.py
@@ -29,39 +29,14 @@ X[:,1]=labelencoder_X.fit_transform(X[:,1])
 
 #Feature Scalling ploting the data with in the same range
 
-
 #Divide train and test the model
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train,y_test=train_test_split(X, y, test_size=0.35, random_state=0)
+X_train, X_test, y_train,y_test=train_test_split(X, y, test_size=0.29, random_state=0)
 
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train=sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
-
-
-
-# Create the mpodel
-
-from sklearn.tree import DecisionTreeClassifier
-
-classifier=DecisionTreeClassifier(criterion="entropy", random_state=0)
-classifier.fit(X_train, y_train)
-
-y_pred=classifier.predict(X_test)
-
-
-from sklearn.metrics import confusion_matrix,accuracy_score, classification_report
-cm=confusion_matrix(y_test,y_pred)
-
-accuracy=accuracy_score(y_test,y_pred)
-clas=classification_report(y_test,y_pred)
-
-
-from sklearn.preprocessing import StandardScaler
-sc=StandardScaler()
-X_train=sc.fit_transform(X_train)
-X_test=sc.transform(X_test)
 
 # Create the mpodel
 
